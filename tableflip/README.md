e0526a52bea52676d6dfc131e6403e9a1c36b192
Wang Qi <ericwq057@qq.com>
2021-11-09 13:26:19 +0800

    add the tcp restart example

---
+++ tableflip/README.md
@@ -0,0 +1,47 @@
+# Demo steps
+
+Run the following command to build the binaraies.
+
+- `go build -o demo tcp.go`
+
+Run the following command to start the server.
+
+- `./demo`
+
+Run the following command to start the client.
+
+- `while true; do nc localhost 8080 -e echo hello; sleep 0.1; done`
+
+The server side log is as following.
+
+```sh
+$ ./demo
+[30377] 2021/11/09 13:22:43 listening on  127.0.0.1:8080
+[30377] 2021/11/09 13:22:43 receive message:[hello]
+[30377] 2021/11/09 13:22:43 receive message:[hello]
+[30377] 2021/11/09 13:22:43 receive message:[hello]
+[30377] 2021/11/09 13:22:43 receive message:[hello]
+[30377] 2021/11/09 13:22:44 receive message:[hello]
+```
+
+Run the following command to restart the server. Note that the [PID] should be replaced with the previous PID.
+
+- `kill -s HUP [PID]`
+
+Watch the output of server side log. You should notice that the server doesn't report any error during the restart.
+
+```sh
+[30377] 2021/11/09 13:23:01 receive message:[hello]
+[30377] 2021/11/09 13:23:01 receive message:[hello]
+[30377] 2021/11/09 13:23:01 receive message:[hello]
+[30720] 2021/11/09 13:23:01 listening on  127.0.0.1:8080
+[30377] 2021/11/09 13:23:01 return from main goroutine.
+[30377] 2021/11/09 13:23:01 receive message:[hello]
+[30377] 2021/11/09 13:23:01 finish the old process.
+ide@golangide:~/proj/examples/tableflip $ [30720] 2021/11/09 13:23:01 receive message:[hello]
+[30720] 2021/11/09 13:23:01 receive message:[hello]
+[30720] 2021/11/09 13:23:01 receive message:[hello]
+[30720] 2021/11/09 13:23:02 receive message:[hello]
+[30720] 2021/11/09 13:23:02 receive message:[hello]
+
+```
---
+++ tableflip/tcp.go
@@ -0,0 +1,130 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"strings"
+	"sync"
+	"syscall"
+	"time"
+
+	"github.com/cloudflare/tableflip"
+)
+
+func main() {
+	Example_tcpServer()
+}
+
+// This shows how to use the Upgrader
+// with a listener based service.
+func Example_tcpServer() {
+	var (
+		listenAddr = flag.String("listen", "localhost:8080", "`Address` to listen on")
+		pidFile    = flag.String("pid-file", "", "`Path` to pid file")
+	)
+
+	flag.Parse()
+	log.SetPrefix(fmt.Sprintf("[%d] ", os.Getpid()))
+
+	/*
+		var lc = &net.ListenConfig{
+			Control: func(network, address string, c syscall.RawConn) error {
+				var opErr error
+				if err := c.Control(func(fd uintptr) {
+					opErr = unix.SetsockoptInt(int(fd), unix.SOL_SOCKET, unix.SO_REUSEPORT, 1)
+				}); err != nil {
+					return err
+				}
+				return opErr
+			},
+		}
+	*/
+
+	upg, err := tableflip.New(tableflip.Options{
+		PIDFile: *pidFile,
+	})
+
+	if err != nil {
+		panic(err)
+	}
+	defer upg.Stop()
+
+	// Do an upgrade on SIGHUP
+	go func() {
+		sig := make(chan os.Signal, 1)
+		signal.Notify(sig, syscall.SIGHUP)
+		for range sig {
+			err := upg.Upgrade()
+			if err != nil {
+				log.Println("upgrade failed:", err)
+			}
+		}
+	}()
+
+	ln, err := upg.Fds.Listen("tcp", *listenAddr)
+	if err != nil {
+		log.Fatalln("Can't listen:", err)
+	}
+
+	var wg sync.WaitGroup
+	quit := make(chan struct{})
+
+	wg.Add(1)
+	go func() {
+		defer func() {
+			log.Println("return from main goroutine.")
+			ln.Close()
+			wg.Done()
+		}()
+
+		log.Println("listening on ", ln.Addr())
+
+		for {
+			select {
+			case <-quit:
+				return
+			default:
+			}
+			c, err := ln.Accept()
+			if err != nil {
+				log.Println("Accept return error, stop accept. err:", err)
+				return
+			}
+
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+
+				c.SetDeadline(time.Now().Add(time.Second))
+
+				//read message from the client and print it on screen
+				var buf []byte = make([]byte, 32)
+				_, err := c.Read(buf)
+				if err != nil {
+					log.Println("read from connection error:", err)
+				} else {
+					msg := strings.ReplaceAll(string(buf), "\n", "")
+					log.Printf("receive message:[%s]", msg)
+				}
+				//c.Write([]byte("It is a mistake to think you can solve any major problems just with potatoes.\n"))
+				c.Close()
+			}()
+		}
+	}()
+
+	if err := upg.Ready(); err != nil {
+		panic(err)
+	}
+	<-upg.Exit()
+
+	close(quit)
+
+	wg.Wait()
+	log.Println("finish the old process.")
+
+	//log.Println("Exit() pause for a moment.")
+	//time.Sleep(time.Second * 2)
+}
