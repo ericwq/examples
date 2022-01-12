acee0c8925e822ae322eed9362ad09cffab2e472
Qi Wang <ericwq057@qq.com>
2022-01-12 13:10:29 +0800

    formating lspconfig and refine gopls with example

--- neovim/custom/plugins/lspconfig.lua
+++ neovim/custom/plugins/lspconfig.lua
@@ -3,53 +3,48 @@
 -----------------------------------------------------------
 -- Plugin: nvim-lspconfig
 -- for language server setup see: https://github.com/neovim/nvim-lspconfig
+-- https://github.com/ChrisAmelia/dotfiles/blob/master/nvim/lua/lsp.lua#L108-L120
 --
 local M = {}
 
 M.setup_lsp = function(attach, capabilities)
-    local lspconfig = require "lspconfig"
+	local lspconfig = require "lspconfig"
 
-    --[[
-   -- lspservers with default config
-   -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+	-- c/c++ language server
+	lspconfig.clangd.setup {}
 
-   local servers = { "gopls", "clangd", "sumneko_lua" }
+	-- go language server
+	-- cmd = {"gopls", "-remote", "auto", "-logfile", "/tmp/gopls.log"},
+	lspconfig.gopls.setup {
+		cmd = {
+			"gopls",
+			"serve",
+		},
+		on_attach = attach,
+		-- on_attach = on_attach_vim,
+		capabilities = capabilities,
+		settings = {
+			gopls = {
+				analyses = {
+					unusedparams = true,
+				},
+				staticcheck = true,
+				linksInHover = false,
+				codelenses = {
+					generate = true,
+					gc_details = true,
+					regenerate_cgo = true,
+					tidy = true,
+					upgrade_depdendency = true,
+					vendor = true,
+				},
+				usePlaceholders = true,
+				gofumpt = true,
+			},
+		},
+	}
 
-   -- Set settings for language servers below
-
-   local ts_settings = function(client)
-     client.resolved_capabilities.document_formatting = true
-     --ts_settings(client)
-   end
-
-   for _, lsp in ipairs(servers) do
-      lspconfig[lsp].setup {
-         on_attach = attach,
-         capabilities = capabilities,
-	 ts_settings = ts_settings,
-         flags = {
-            debounce_text_changes = 150,
-         },
-      }
-   end
---]]
-    lspconfig.clangd.setup {}
-
-    lspconfig.gopls.setup {
-      --cmd = {"gopls", "-remote", "auto", "-logfile", "/tmp/gopls.log"},
-      cmd = {"gopls", "serve"},
-      on_attach = attach,
-      capabilities = capabilities,
-      settings = {
-        gopls = {
-          gofumpt = true,
-          usePlaceholders = true,
-          staticcheck = true
-        }
-      }
-    }
-
---[[
+	--[[
     lspconfig.efm.setup {
 		on_attach = attach,
 		capabilities = capabilities,
@@ -69,41 +64,45 @@ M.setup_lsp = function(attach, capabilities)
         }
     }
 ]]
-    -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#sumneko_lua
-    local runtime_path = vim.split(package.path, ';')
-    table.insert(runtime_path, "lua/?.lua")
-    table.insert(runtime_path, "lua/?/init.lua")
+	-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#sumneko_lua
+	local runtime_path = vim.split(package.path, ';')
+	table.insert(runtime_path, "lua/?.lua")
+	table.insert(runtime_path, "lua/?/init.lua")
 
-    lspconfig.sumneko_lua.setup {
+	lspconfig.sumneko_lua.setup {
 		on_attach = attach,
 		capabilities = capabilities,
 		root_dir = lspconfig.util.root_pattern('.git'),
-        -- function(client, bufnr)
-        --     client.resolved_capabilities.document_formatting = false
-        --     vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>fm", "<cmd>lua vim.lsp.buf.formatting()<CR>", {})
-        -- end,
-        settings = {
-            Lua = {
-                runtime = {
-                    -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                    version = 'LuaJIT',
-                    -- Setup your lua path
-                    path = runtime_path
-                },
-                diagnostics = {
-                    -- Get the language server to recognize the `vim` global
-                    globals = {'vim'}
-                },
-                workspace = {
-                    -- Make the server aware of Neovim runtime files
-                    library = vim.api.nvim_get_runtime_file("", true)
-                },
-                -- Do not send telemetry data containing a randomized but unique identifier
-                telemetry = {enable = false}
-            }
-        }
-    }
-    --[[
+		-- function(client, bufnr)
+		--     client.resolved_capabilities.document_formatting = false
+		--     vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>fm", "<cmd>lua vim.lsp.buf.formatting()<CR>", {})
+		-- end,
+		settings = {
+			Lua = {
+				runtime = {
+					-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+					version = 'LuaJIT',
+					-- Setup your lua path
+					path = runtime_path,
+				},
+				diagnostics = {
+					-- Get the language server to recognize the `vim` global
+					globals = {
+						'vim',
+					},
+				},
+				workspace = {
+					-- Make the server aware of Neovim runtime files
+					library = vim.api.nvim_get_runtime_file("", true),
+				},
+				-- Do not send telemetry data containing a randomized but unique identifier
+				telemetry = {
+					enable = false,
+				},
+			},
+		},
+	}
+	--[[
 --https://github-wiki-see.page/m/jose-elias-alvarez/null-ls.nvim/wiki/Avoiding-LSP-formatting-conflicts
    -- typescript
 
