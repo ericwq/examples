e2b7a2d65a266569d5227d8e7b158241ca5f1654
ericwq <ericwq057@qq.com>
2023-08-08 07:49:55 +0800

    - refine log example.

--- log/log.go
+++ log/log.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"log/syslog"
-	"net"
+	"os"
+	"time"
 
 	"golang.org/x/exp/slog"
 )
@@ -42,27 +41,44 @@ func main() {
 			).
 			Info("user registration")
 	*/
-	writer, err := net.Dial("udp", "localhost:514")
-	if err != nil {
-		fmt.Println(err)
-	}
+	// writer, err := net.Dial("udp", "localhost:514")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	writer := os.Stdout
 
 	if writer != nil {
 		// defer writer.Close()
-		textHandler := slog.NewTextHandler(writer, nil)
+		textHandler := slog.NewTextHandler(writer, &slog.HandlerOptions{AddSource: true, Level: slog.LevelDebug})
 		logger := slog.New(textHandler)
 
-		logger.Warn("from slog!")
+		logger.Debug("from slog!")
 		logger.Info("be careful about 70 handcup!")
+
+		logger.
+			With("category", "sql").
+			With("query.statement", "SELECT COUNT(*) FROM users;").
+			With("query.duration", 1*time.Second).
+			With("error", fmt.Errorf("could not count users")).
+			Error("caramba!")
+
+		logger.
+			With(
+				slog.Group("user",
+					slog.String("id", "user-123"),
+					slog.Time("created_at", time.Now()),
+				),
+			).
+			Info("user registration")
 	}
 
 	// here is the syslog sample
-	s, err := syslog.New(syslog.LOG_WARNING|syslog.LOG_LOCAL7, "aprilsh")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	s.Warning("from syslog")
-	s.Info("I am 98 handcup.")
+	// s, err := syslog.New(syslog.LOG_WARNING|syslog.LOG_LOCAL7, "aprilsh")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	//
+	// s.Warning("from syslog")
+	// s.Info("I am 98 handcup.")
 
 }
